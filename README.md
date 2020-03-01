c2203e93e6e9a92623cd757066c9213dfd1b9320
Scotty Rogers <scotty.rogers10@gmail.com>
2020-02-29 18:42:29 -0700

    Updated package dependencies.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # generic-data-chamber
-A global data chamber that is library agnostic.
+
+![NPM version](https://img.shields.io/npm/v/generic-data-chamber.svg?style=flat)
+![NPM license](https://img.shields.io/npm/l/generic-data-chamber.svg?style=flat)
+![NPM total downloads](https://img.shields.io/npm/dt/generic-data-chamber.svg?style=flat)
+![NPM monthly downloads](https://img.shields.io/npm/dm/generic-data-chamber.svg?style=flat)
+
+A global data store that is library agnostic.
+
+## Install
+
+```bash
+npm install generic-data-chamber
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
+const path = require("path");
+
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    library: "DataChamber",
-    libraryTarget: "umd"
+    library: "dataChamber",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    globalObject: "this"
   },
   module: {
     rules: [
       {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
           options: {
@@ -21,8 +24,5 @@ module.exports = {
         }
       }
     ]
-  },
-  resolve: {
-    extensions: ["*", ".js"]
   }
 };
